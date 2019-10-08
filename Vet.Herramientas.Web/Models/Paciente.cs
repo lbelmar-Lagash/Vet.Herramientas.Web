9802d31b0b01092c70da1779d2058399e041fbe0
leonardbelmar <leonardbelmar@gmail.com>
2019-10-08 12:17:55 -0300

    Initial commit

---
+++ Vet.Herramientas.Web/Models/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vet.Herramientas.Web.Models
+{
+	public class Paciente
+	{
+		public string Nombre { get; set; }
+		public string Especie { get; set; }
+		public string Chip { get; set; }
+		public string Raza { get; set; }
+		public string Color { get; set; }
+		public string Sexo { get; set; }
+		public float Peso { get; set; }
+		public DateTime Nacimiento { get; set; }
+	}
+}
---
+++ Vet.Herramientas.Web/Models/Propietario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vet.Herramientas.Web.Models
+{
+	public class Propietario
+	{
+		public string Nombre { get; set; }
+		public string Domicilio { get; set; }
+		public string Telefono { get; set; }
+		public string Nombre { get; set; }
+	}
+}
