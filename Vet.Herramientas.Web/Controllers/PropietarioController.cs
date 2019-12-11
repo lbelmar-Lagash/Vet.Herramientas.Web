13893d468c67aae176bff71ed9cb8c258a441738
leonardbelmar <leonardbelmar@gmail.com>
2019-12-11 14:30:08 -0300

    Se crea formulario que ingresa datos vía Dapper

--- Vet.Herramientas.Web/Controllers/PropietarioController.cs
+++ Vet.Herramientas.Web/Controllers/PropietarioController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Vet.Herramientas.Web.Models;
@@ -23,13 +24,32 @@ namespace Vet.Herramientas.Web.Controllers
 
 		[HttpPost]
 		public IActionResult Ingresar(Propietario propietario)
-		{	
+		{
+			using (var connection = new MySqlConnection("Server=localhost; Port=3306; Database = arauco-dev-db; Uid = root; Pwd =arauco-dev-db;  Uid = root; Pwd = arauco-password; "))
+			{
+				var parameters = new DynamicParameters();
+				parameters.Add("@nombre", propietario.Nombre);
+				parameters.Add("@apellido", propietario.ApellidoPaterno);
+				parameters.Add("@apellidopaterno", propietario.ApellidoMaterno);
+
+				connection.Execute(@"insert into Propietario values (@nombre, @apellido, @apellidopaterno)", parameters);
+			}
 			return View("Detalle", propietario);
 		}
 
 		public IActionResult Listar()
 		{
-			return View("List", PropietarioService.ObtenerPropietarios());
+			var propietarios = new List<Propietario>();
+
+			using (var connection = new MySqlConnection("Server=localhost; Port=3306; Database = arauco-dev-db; Uid = root; Pwd =arauco-dev-db;  Uid = root; Pwd = arauco-password; "))
+			{
+				propietarios = connection.Query<Propietario>(@"select nombre, 
+												apellido_paterno as ApellidoPaterno,
+												apellido_materno as ApellidoMaterno
+												from Propietario").ToList();
+			}
+
+			return View("List", propietarios);
 		}
 
 	}
